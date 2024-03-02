44d68054cd1ab760bf1b87e0fae2a59ede85f4a8
uday <uswagh4888@gmail.com>
2024-03-02 12:49:21 +0530

    develop branch changes

--- 06_Web_MVC_Student_App/src/main/java/com/ct/Application.java
+++ 06_Web_MVC_Student_App/src/main/java/com/ct/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
+		System.out.println("This is for branch change develop");
+
 		SpringApplication.run(Application.class, args);
 	}
 
